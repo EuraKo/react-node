aeaecaa874aa77f9d8853954ed33508ca5343596
EuraKo <sow5252@gmail.com>
2022-05-25 15:13:40 +0900

    컴포넌트 마운트시 로그인정보확인 및 강제 로그아웃 처리

--- react/src/App.js
+++ react/src/App.js
@@ -1,4 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { loginUser, logoutUser } from './redux/userSlice';
+import firebase from './firebase';
+
 import Header from './component/Header';
 import Main from './component/Main';
 import List from './component/post/List';
@@ -9,8 +13,22 @@ import Login from './component/user/Login';
 import Join from './component/user/Join';
 
 import './scss/style.scss';
+import { useEffect } from 'react';
 
 function App() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		// auth상태 변화를 감지해서 인수로 해당 상태값을 전달
+		firebase.auth().onAuthStateChanged((userInfo) => {
+			console.log('userInfo : ', userInfo);
+		});
+	}, []);
+
+	// 로그아웃 테스트
+	useEffect(() => {
+		firebase.auth().signOut();
+	}, []);
 	return (
 		<>
 			<Header />
