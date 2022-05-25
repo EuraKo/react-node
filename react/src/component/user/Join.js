5712ffcc3898f18de8ef1f66a97955c2bf305125
EuraKo <sow5252@gmail.com>
2022-05-25 09:14:18 +0900

    로그인 컴포넌트 추가

---
+++ react/src/component/user/Join.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Join() {
+	return <div>Join</div>;
+}
+
+export default Join;
---
+++ react/src/component/user/Login.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+	const navigate = useNavigate();
+	const [email, setEmail] = useState('');
+	const [pw, setPw] = useState('');
+
+	return (
+		<section id='login'>
+			<article>
+				<div className='inner'>
+					<h2>Login</h2>
+
+					<input
+						type='email'
+						value={email}
+						placeholder='이메일을 입력하세요'
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<br />
+					<input
+						type='password'
+						value={pw}
+						placeholder='비밀번호를 입력하세요'
+						onChange={(e) => setPw(e.target.value)}
+					/>
+					<br />
+					<button>login</button>
+					<button
+						onClick={() => {
+							navigate('/join');
+						}}>
+						join
+					</button>
+				</div>
+			</article>
+		</section>
+	);
+}
+
+export default Login;
