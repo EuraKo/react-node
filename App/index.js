4e92fa0a50578dc14dd87a85068bae8c71deb375
EuraKo <sow5252@gmail.com>
2022-05-26 15:06:43 +0900

    heroku배포

--- App/index.js
+++ App/index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const config = require('./config/key.js'); // 배포와 개발을 나눠 몽고디비 정보 숨기기
+const config = require('./server/config/key.js'); // 배포와 개발을 나눠 몽고디비 정보 숨기기
 const app = express();
 const port = 5000;
 
 // express에서 react폴더 안쪽에 react폴더를 static(추가가공없다는뜻)으로 지정
-app.use(express.static(path.join(__dirname, '../react/build')));
+app.use(express.static(path.join(__dirname, './react/build')));
 app.use('/img', express.static('./img'));
 // client로 부터 전달받을 내용을 불러오기 위한 body-parser설정
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // post 전용 라우터 추가
-app.use('/api/post', require('./router/post.js'));
+app.use('/api/post', require('./server/router/post.js'));
 // user 전용 라우터
-app.use('/api/user', require('./router/user.js'));
+app.use('/api/user', require('./server/router/user.js'));
 
 // 터미널에 뜨는 문구
 app.listen(port, () => {
@@ -35,12 +35,12 @@ app.listen(port, () => {
 // localhost:5000번에 뜨는 내용
 app.get('/', (req, res) => {
 	// 서버에서 5000번 포트로 접속하면 static폴더로 지정되어 있는 build안쪽의 index.html을 화면에 내보냄
-	res.sendFile(path.join(__dirname, '../react/build/index.html'));
+	res.sendFile(path.join(__dirname, './react/build/index.html'));
 });
 
 // 어떤 경로에서 접속하든 index.html이 뜨도록한다. 이걸 안하면 루트페이지가 아닌 페이지에서 새로고침하면 에러가 뜸
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../react/build/index.html'));
+	res.sendFile(path.join(__dirname, './react/build/index.html'));
 });
 
 /* //클라이언트로 부터 넘어온 요청처리 라우터
---
+++ App/server/config/product.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI: process.env.MONGO_URI, // MONGO_URI는 내가 지은이름으로 heroku Reveal Config Vars에 등록할 key이름 value는 monggo uri 값을 넣어준다.
+};
